0793eb7909839729714295f4549f17030726acd4
Makon Cline <makoncline@gmail.com>
2022-05-14 14:55:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+function main() {
+  const input = document.querySelector("input");
+  input.addEventListener("change", handleSelectFile);
+}
+
+function handleSelectFile(e) {
+  const files = e.target.files;
+  if (!files.length) {
+    alert("No file selected");
+  }
+  var fr = new FileReader();
+  fr.onload = function () {
+    const items = processData(fr.result);
+    displayItems(items);
+  };
+  fr.readAsText(files[0]);
+}
+
+function processData(data) {
+  const lines = data.split("\n");
+  const headers = lines[0].split(",");
+  const dataRows = lines.slice(1);
+  const dataArray = dataRows.map((row) => {
+    const values = row.split(",");
+    const dataObj = {};
+    for (let i = 0; i < headers.length; i++) {
+      dataObj[headers[i]] = values[i];
+    }
+    return dataObj;
+  });
+  return dataArray;
+}
+
+function displayItems(items) {
+  const labelsPerPage = 30;
+  const labelPages = [];
+  for (let i = 0; i < items.length; i += labelsPerPage) {
+    const labels = items.slice(i, i + labelsPerPage);
+    labelPages.push(labels);
+  }
+  const pageContainer = document.querySelector("#page-container");
+  pageContainer.innerHTML = "";
+  labelPages.slice(0, 1).forEach((labelPage) => {
+    let labelHtml = "";
+    labelPage.forEach((label) => {
+      labelHtml += `
+      <div class="label">
+        <div class="title">${label["Name"]}</div>
+        <div class="bin">${label["Bin Number"]}</div>
+        <div class="description">${label["Description"]}</div>
+      </div>
+      `;
+    });
+    pageContainer.innerHTML += `
+    <div class="page">
+      <div class="grid">${labelHtml}</div>
+    </div>
+    `;
+  });
+}
+
+function truncate(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+
+main();
